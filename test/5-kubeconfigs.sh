b9a2a1877f30225138820152d9a7576778f5a844
Marius Kriegerowski <marius.kriegerowski@gmail.com>
2023-12-11 14:56:05 +0100

    fix kubeconfigs

--- test/5-kubeconfigs.sh
+++ test/5-kubeconfigs.sh
@@ -1,3 +1,11 @@
+for i in 0 1 2; do
+  export WORKER${i}=$(hcloud server list --selector "tag=worker-${i}" -o columns=ipv4 -o noheader)
+done
+for i in 0 1 2; do
+  export CONTROLLER${i}=$(hcloud server list --selector "tag=controller-${i}" -o columns=ipv4 -o noheader)
+done
+echo $CONTROLLER0 $CONTROLLER1 $CONTROLLER2 $WORKER0 $WORKER1 $WORKER2
+
 KUBERNETES_PUBLIC_ADDRESS=$(hcloud load-balancer list --selector tag=kubernetes-the-hard-way -o columns=ipv4 -o noheader)
 for instance in worker-0 worker-1 worker-2; do
   kubectl config set-cluster kubernetes-the-hard-way \
