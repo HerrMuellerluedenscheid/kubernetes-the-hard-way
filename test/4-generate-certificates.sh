8ddc7caa93e119c98e18a14fce806a4f4466143d
Marius Kriegerowski <marius.kriegerowski@gmail.com>
2023-12-11 14:55:43 +0100

    fix auto generation

--- test/4-generate-certificates.sh
+++ test/4-generate-certificates.sh
@@ -1,3 +1,11 @@
+for i in 0 1 2; do
+  export WORKER${i}=$(hcloud server list --selector "tag=worker-${i}" -o columns=ipv4 -o noheader)
+done
+for i in 0 1 2; do
+  export CONTROLLER${i}=$(hcloud server list --selector "tag=controller-${i}" -o columns=ipv4 -o noheader)
+done
+echo $CONTROLLER0 $CONTROLLER1 $CONTROLLER2 $WORKER0 $WORKER1 $WORKER2
+
 {
 
 cat > ca-config.json <<EOF
